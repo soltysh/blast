eac052ee0b147789e79e69582d65ec74bfd0b9ba
Maciej Szulik <soltysh@gmail.com>
2017-05-06 10:27:57 +0200

    Scraper - cleaning code, adding error handling and comments

--- scraper/app.py
+++ scraper/app.py
@@ -1,14 +1,14 @@
 import random
 import os
-
+import traceback
 import kubernetes.client as k8s
 
 from os.path import getsize
 from scrapy.crawler import CrawlerProcess
-from spider import DataSpider, TextItem
+from spider import DataSpider
 
 WORDS_FILE = 'words.txt'
-
+SERVICEACCOUNT_LOCATION = '/var/run/secrets/kubernetes.io/serviceaccount/'
 
 def random_word():
     """
@@ -24,27 +24,25 @@ def read_file(name):
     """
     Reads configuration from serviceaccounts files.
     """
-    with open('/var/run/secrets/kubernetes.io/serviceaccount/' + name) as f:
+    with open(SERVICEACCOUNT_LOCATION + name) as f:
         return f.read()
 
 def read_configmap():
     """
     Reads ConfigMap object storing secrets to databases.
     """
+    namespace = read_file('namespace')
+    k8s.configuration.host = 'https://{}:{}'.format(
+        os.getenv('KUBERNETES_SERVICE_HOST'),
+        os.getenv('KUBERNETES_SERVICE_PORT'))
+    k8s.configuration.api_key_prefix['authorization'] = 'Bearer'
+    k8s.configuration.api_key['authorization'] = read_file('token')
+    k8s.configuration.ssl_ca_cert = SERVICEACCOUNT_LOCATION + 'ca.crt'
+    # we assume the ConfigMap name is 'config'
+    obj = k8s.CoreV1Api().read_namespaced_config_map('config', namespace)
 
-        namespace = read_file('namespace')
-        k8s.configuration.host = 'https://{}:{}'.format(
-            os.getenv('KUBERNETES_SERVICE_HOST'),
-            os.getenv('KUBERNETES_SERVICE_PORT'))
-        k8s.configuration.api_key_prefix['authorization'] = 'Bearer'
-        k8s.configuration.api_key['authorization'] = read_file('token')
-        k8s.configuration.ssl_ca_cert = '/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'
-        # we assume the ConfigMap name is 'config'
-        obj = k8s.CoreV1Api().read_namespaced_config_map('config', namespace)
-        return obj.data['text_db_user'], \
-            obj.data['text_db_pass'], \
-            os.getenv('TEXT_DB_SERVICE_HOST'), \
-            os.getenv('TEXT_DB_SERVICE_PORT')
+    return obj.data['text_db_user'], obj.data['text_db_pass'], \
+        os.getenv('TEXT_DB_SERVICE_HOST'), os.getenv('TEXT_DB_SERVICE_PORT')
 
 
 if __name__ == '__main__':
@@ -52,9 +50,9 @@ if __name__ == '__main__':
     if 'KUBERNETES_SERVICE_HOST' in os.environ:
         try:
             user, passwd, host, port = read_configmap()
-        except k8s.rest.ApiException:
-            print(e, file=sys.stderr)
-            return
+        except k8s.rest.ApiException as e:
+            print("Error reading config map!", file=sys.stderr)
+            traceback.print_exc(file=sys.stderr)
     # create the spider
     word = random_word()
     process = CrawlerProcess()
--- scraper/spider.py
+++ scraper/spider.py
@@ -3,7 +3,6 @@ import sys
 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from scrapy import Item, Field
 from scrapy.linkextractors import LinkExtractor
 from scrapy.selector import Selector
 from scrapy.spiders import CrawlSpider, Rule
@@ -11,11 +10,10 @@ from scrapy.spiders import CrawlSpider, Rule
 GOOGLE_RE = re.compile('https://www.google\.\w{2,3}')
 
 
-class TextItem(Item):
-    url = Field()
-
-
 class DataSpider(CrawlSpider):
+    """
+    Spider responsible for gathering urls matching given word.
+    """
     name = 'data_spider'
 
     rules = (
@@ -25,21 +23,32 @@ class DataSpider(CrawlSpider):
     def __init__(self, textdb=None, *a, **kw):
         super(DataSpider, self).__init__(*a, **kw)
         try:
-            self._client = MongoClient('mongodb://{user}:{passwd}@{host}:{port}/blast_text'
+            self.client = MongoClient('mongodb://{user}:{passwd}@{host}:{port}/blast_text'
                 .format(**textdb))
-            self._client.server_info()
+            self.client.server_info()
         except ServerSelectionTimeoutError:
             self.logger.error("Could not connect to mongo (timeout), nothing will be saved!")
-            self._client = None
+            self.client = None
 
     def parse_item(self, response):
+        """
+        Parses single item.
+        """
         if GOOGLE_RE.match(response.url):
             return
         selector = Selector(response)
         title = selector.xpath('//title/text()').extract()[0]
-        self.logger.info("Saving %s - %s", title, response.url)
-        if self._client:
+        self.save_item(title, response.url)
+
+    def save_item(self, text, url):
+        """
+        Saves single item in database.
+        """
+        self.logger.info("Saving %s - %s", text, url)
+        if self.client:
             try:
-                self._client.blast_text.text.insert_one({'text': title, 'url': response.url})
+                self.client.blast_text.text.insert_one({'text': text, 'url': url})
             except Exception as e:
                 self.logger.error("Could not save item: %s", e)
+        else:
+            self.logger.error("Could not save item: db connection not active!")
