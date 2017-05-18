558094fa50a4ad414587a71ec128bd5cb88e2c29
Maciej Szulik <soltysh@gmail.com>
2017-05-17 21:50:29 -0700

    Added CronJob to the template and setup service account

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# create all the templates for each
+# create service account for the cron job
+oc create serviceaccount scraper
+oc policy add-role-to-user view -z scraper
+
+# create all the templates for each component
 for dir in $(find * -maxdepth 0 -type d)
 do
     oc new-app ${dir}/template.yaml
